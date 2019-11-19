cd7e2e75f3cb489b9c9e04c661700099727ff7ee
Ferdynal <ferdynallubis@gmail.com>
2019-11-19 20:49:51 +0700

    repaired signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,8 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const sendEmail = require('./../utils/email');
 const Email = require('./../utils/email');
+// const sendEmail = require('./../utils/email');
 
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
